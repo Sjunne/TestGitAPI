f60e3d01446d62aeed21679f9f642c7035abc4ee
Sjunne <mattias.leth@lunar.app>
2022-09-26 12:34:57 +0200

    test

---
+++ Test.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
+
+type webhook struct {
+	Action     string
+	Repository struct {
+		ID       string
+		FullName string
+	}
+}
+
+func handleWebhook(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("headers: %v\n", r.Header)
+
+	_, err := io.Copy(os.Stdout, r.Body)
+	if err != nil {
+		fmt.Printf(err.Error())
+		return
+	}
+}
+
+func main() {
+	log.Println("server started")
+	http.HandleFunc("/webhook", handleWebhook)
+	log.Fatal(http.ListenAndServe(":80", nil))
+}
